501064c07754e119ec100b710abac5da3477b47b
gabafo1 <gabrielafolabi@gmail.com>
2023-11-28 16:15:18 -0800

    python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) not in ['q', 'e']:
-        print(f"{chr(i)}", end='')
+for t in range(97, 123, 1):
+    if t == 113 or t == 101:
+        pass
+    else:
+        print("{}".format(chr(t)), end="")
