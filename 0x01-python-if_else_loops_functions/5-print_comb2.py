3b686b2d5c4288251d1b0fc7d0b77675c4898c98
gabafo1 <gabrielafolabi@gmail.com>
2023-11-28 16:28:25 -0800

    python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-for i in range(100): print("{:02d}{}".format(i, ', ' if i < 99 else '\n'), end='')
+for i in range(100): 
+    print("{:02d}{}".format(i, ', ' if i < 99 else '\n'), end='')
