fef0eabdbbeba17a6ed0f449078f98c0df08fab0
gabafo1 <gabrielafolabi@gmail.com>
2023-11-28 16:50:56 -0800

    python

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-def pow(a, b):
-    result = 1
-
-    if b < 0:
-        a = 1 / a
-        b = -b
-
-    for _ in range(b):
-        result *= a
-
-    return result
-
+def pow(a,b):
+    return (a ** b)
