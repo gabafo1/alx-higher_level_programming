293d485d502285f62af19ed0703a43e133bfafff
gabafo1 <gabrielafolabi@gmail.com>
2023-12-04 16:22:15 -0800

    python

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    # Check if the list is empty
-    if not my_list:
-        print("The list is empty.")
-    else:
-        # Loop through each element in the list
-        for x in my_list:
-            # Use str.format() to print the integer
-            print("{:d}".format(x))
+    for num in my_list:
+        print("{:d}".format(num))
