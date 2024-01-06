22e0fa6de1615329572247ce24e7d935d8f16c90
gabafo1 <gabrielafolabi@gmail.com>
2024-01-06 15:49:54 -0800

    python

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,7 @@
 Defining a class Rectangle
 """
 
+
 class Rectangle:
     """A class that defines a rectangle.
 
@@ -65,6 +66,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+
 if __name__ == "__main__":
     my_rectangle = Rectangle(2, 4)
     print(my_rectangle.__dict__)
@@ -72,4 +74,3 @@ if __name__ == "__main__":
     my_rectangle.width = 10
     my_rectangle.height = 3
     print(my_rectangle.__dict__)
-
